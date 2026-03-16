d23879645433e315f9a4b16db4873f23a0ab5a64
Allan Braquiel <alnbraquiel@gmail.com>
2026-03-16 14:05:00 -0300

    Teste de integração 02

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,49 +1,49 @@
 <x-app-layout>
 
-<x-slot name="header">
-<h2 class="h4">Dashboard</h2>
-</x-slot>
+    <x-slot name="header">
+        <h2 class="h4">Dashboard</h2>
+    </x-slot>
 
-<div class="container mt-4">
+    <div class="container mt-4">
 
-<div class="row">
+    <div class="row">
 
-<div class="col-md-6">
+    <div class="col-md-6">
 
-<div class="card shadow">
-<div class="card-body text-center">
+    <div class="card shadow">
+    <div class="card-body text-center">
 
-<h5 class="card-title">👤 Pessoas</h5>
-<p class="card-text">Gerenciar Cadastro de Pessoas</p>
+    <h5 class="card-title">👤 Pessoas</h5>
+    <p class="card-text">Gerenciar Cadastro de Pessoas</p>
 
-<a href="/pessoas" class="btn btn-primary">
-Acessar
-</a>
-
-</div>
-</div>
-
-</div>
-
-<div class="col-md-6">
-
-<div class="card shadow">
-<div class="card-body text-center">
-
-<h5 class="card-title">📦 Produtos</h5>
-    <p class="card-text">Gerenciar Cadastro de Produtos</p>
-
-<a href="/produtos" class="btn btn-success">
+    <a href="/pessoas" class="btn btn-primary">
     Acessar
-</a>
+    </a>
 
-</div>
-</div>
+    </div>
+    </div>
 
-</div>
+    </div>
 
-</div>
+    <div class="col-md-6">
 
-</div>
+    <div class="card shadow">
+    <div class="card-body text-center">
+
+    <h5 class="card-title">📦 Produtos</h5>
+        <p class="card-text">Gerenciar Cadastro de Produtos</p>
+
+    <a href="/produtos" class="btn btn-success">
+        Acessar
+    </a>
+
+    </div>
+    </div>
+
+    </div>
+
+    </div>
+
+    </div>
 
 </x-app-layout>
