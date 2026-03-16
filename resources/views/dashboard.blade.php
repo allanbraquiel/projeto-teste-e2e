39829691249844c98a3698379588bedded6fb2dd
Allan Braquiel <alnbraquiel@gmail.com>
2026-03-16 15:35:33 -0300

    Teste de integração 08

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,8 @@
     </x-slot>
 
     <div class="container mt-4">
-
         <div class="row">
-
             <div class="col-md-6">
-
         <div class="card shadow">
     <div class="card-body text-center">
 
